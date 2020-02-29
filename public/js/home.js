cb73c8f006c8db296ec9b9559709dc107701886f
zphoenixz <ing.ramiro.valdez.a@gmail.com>
2020-02-29 04:24:16 -0400

    Final fido me buttons

---
+++ public/js/home.js
@@ -0,0 +1,6 @@
+$(".button-title").click(function () {
+    $('.f.transform').toggleClass('transform-active-f');
+    $('.m.transform').toggleClass('transform-active-m');
+});
+
+jQuery("h1").fitText(0.38);
