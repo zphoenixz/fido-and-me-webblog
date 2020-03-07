4b270e4787e85a50e5db718c390c3b33dea38cc1
zphoenixz <ing.ramiro.valdez.a@gmail.com>
2020-03-07 16:21:25 -0400

    background animation

--- public/js/nav_animations.js
+++ public/js/nav_animations.js
@@ -1,7 +1,15 @@
-function navAnimate(name, animation, direction, opacity) {
+function navAnimate(name, animation, direction, tieDirection, opacity) {
     $("#menu0" + name).css("opacity", opacity);
     $("#menu1" + name).css("opacity", opacity);
     $("#menu2" + name).css("opacity", opacity);
+
+    $('#mf-tie').playKeyframe({
+        name: 'tieRotation',
+        duration: "2000ms",
+        delay: "0ms",
+        timingFunction: "ease",
+        direction: tieDirection
+    });
 
     $('#menu0' + name).playKeyframe({
         name: animation,
@@ -26,8 +34,8 @@ function navAnimate(name, animation, direction, opacity) {
     });
 }
 
-$.keyframe.define({
-    name: 'entradaFido',
+$.keyframe.define([{
+    name: 'navBarAnim',
     from: {
         'transform': 'translateX(100%)',
         'opacity': '0'
@@ -36,27 +44,89 @@ $.keyframe.define({
         'transform': 'translateX(0px)',
         'opacity': '1'
     }
-});
+}, {
+    name: 'tieRotation',
+    from: {
+        'transform': 'rotate(0deg)',
+    },
+    to: {
+        'transform': 'rotate(900deg)',
+    }
+}, {
+    name: 'tieFirstRotation1',
+    '0%': {
+        'transform': 'rotate(180deg)'
+    },
+    '25%': {
+        'transform': 'rotate(210deg)'
+    },
+    '50%': {
+        'transform': 'rotate(150deg)'
+    },
+    '100%': {
+        'transform': 'rotate(180deg)'
+    }
+}, {
+    name: 'tieFirstRotation2',
+    '0%': {
+        'transform': 'rotate(0deg)'
+    },
+    '25%': {
+        'transform': 'rotate(30deg)'
+    },
+    '50%': {
+        'transform': 'rotate(-30deg)'
+    },
+    '100%': {
+        'transform': 'rotate(0deg)'
+    }
+}, {
+    name: 'backgroundChange',
+    from: {
+        'box-shadow': 'inset 0 0 0 1000px rgba(59, 76, 102, 0.6)',
+    },
+    to: {
+        'box-shadow': 'inset 0 0 0 1000px rgba(42, 58, 59, 0.5)',
+        
+    }
+}]);
 
-navAnimate('f', 'entradaFido', 'normal', "0");
+navAnimate('f', 'navBarAnim', 'normal', 'normal', "0");
+
+$('#mf-tie').playKeyframe({
+    name: 'tieFirstRotation1',
+    duration: "1000ms",
+    iterationCount: 'infinite',
+    delay: "0ms",
+    timingFunction: "ease",
+    direction: 'normal'
+});
 
 
 $(".button-title").click(function () {
     $('.f.transform').toggleClass('transform-active-f');
     $('.m.transform').toggleClass('transform-active-m');
 
-    if ($(window).width() < 500) {
-        console.log("menor!!!!!!!!!!!!")
-    }
+    // if ($(window).width() < 500) {
+    //     console.log("menor!!!!!!!!!!!!")
+    // }
 
     if (!$(".f.transform ").hasClass("transform-active-f")) {
+        $('section#home_f').css("box-shadow", 'inset 0 0 0 1000px rgba(59, 76, 102, 0.6)');
+        $('section#home_f').playKeyframe({
+            name: 'backgroundChange',
+            duration: "1000ms",
+            delay: "50ms",
+            timingFunction: "ease",
+            direction: 'normal'
+        });
 
-        navAnimate('f', 'entradaFido', 'reverse', "1");
+        navAnimate('f', 'navBarAnim', 'reverse', 'reverse', "1");
 
         setTimeout(function () {
             $('#menu0f').attr("id", "menu0m");
             $('#menu0m').playKeyframe({
-                name: 'entradaFido',
+                name: 'navBarAnim',
                 duration: "1000ms",
                 delay: "50ms",
                 timingFunction: "ease",
@@ -67,7 +137,7 @@ $(".button-title").click(function () {
             $('#menu1f').find('img').attr("src", "/images/svg/blog_m.svg");
             $('#menu1f').attr("id", "menu1m");
             $('#menu1m').playKeyframe({
-                name: 'entradaFido',
+                name: 'navBarAnim',
                 duration: "1000ms",
                 delay: "50ms",
                 timingFunction: "ease",
@@ -78,21 +148,38 @@ $(".button-title").click(function () {
             $('#menu2f').find('img').attr("src", "/images/svg/about_m.svg");
             $('#menu2f').attr("id", "menu2m");
             $('#menu2m').playKeyframe({
-                name: 'entradaFido',
+                name: 'navBarAnim',
                 duration: "1000ms",
                 delay: "50ms",
                 timingFunction: "ease",
                 direction: 'normal'
             });
+            $('#mf-tie').playKeyframe({
+                name: 'tieFirstRotation2',
+                duration: "1000ms",
+                iterationCount: 'infinite',
+                delay: "0ms",
+                timingFunction: "ease",
+                direction: 'reverse'
+            });
         }, 2000);
 
     } else {
-        navAnimate('m', 'entradaFido', 'reverse', "1");
+        $('section#home_f').css("box-shadow", 'inset 0 0 0 1000px rgba(42, 58, 59, 0.5)');
+        $('section#home_f').playKeyframe({
+            name: 'backgroundChange',
+            duration: "1000ms",
+            delay: "50ms",
+            timingFunction: "ease",
+            direction: 'reverse'
+        });
+
+        navAnimate('m', 'navBarAnim', 'reverse', 'normal', "1");
 
         setTimeout(function () {
             $('#menu0m').attr("id", "menu0f");
             $('#menu0f').playKeyframe({
-                name: 'entradaFido',
+                name: 'navBarAnim',
                 duration: "1000ms",
                 delay: "50ms",
                 timingFunction: "ease",
@@ -103,7 +190,7 @@ $(".button-title").click(function () {
             $('#menu1m').find('img').attr("src", "/images/svg/blog_f.svg");
             $('#menu1m').attr("id", "menu1f");
             $('#menu1f').playKeyframe({
-                name: 'entradaFido',
+                name: 'navBarAnim',
                 duration: "1000ms",
                 delay: "50ms",
                 timingFunction: "ease",
@@ -114,9 +201,18 @@ $(".button-title").click(function () {
             $('#menu2m').find('img').attr("src", "/images/svg/about_f.svg");
             $('#menu2m').attr("id", "menu2f");
             $('#menu2f').playKeyframe({
-                name: 'entradaFido',
+                name: 'navBarAnim',
                 duration: "1000ms",
                 delay: "50ms",
+                timingFunction: "ease",
+                direction: 'normal'
+            });
+
+            $('#mf-tie').playKeyframe({
+                name: 'tieFirstRotation1',
+                duration: "1000ms",
+                iterationCount: 'infinite',
+                delay: "0ms",
                 timingFunction: "ease",
                 direction: 'normal'
             });
